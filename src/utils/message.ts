e4a335f3e2a19add22a364a3c0023c2935b9e8ef
Luis Antonio Cruz <luis.cruz@boletomovil.com>
2025-08-04 15:06:13 -0600

    fix: try just message

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,10 +1,5 @@
 export const buildMessage = (message: string) : string => {
   return JSON.stringify({
-    RawBT: [
-      { TextLine: { text: "¡Boletomovil Rocks!" } },
-      { QRCode: { data: "https://boletomovil.com" } },
-      { FeedLine: { lines: 2 } },
-      { CutPaper: {} }
-    ]
+    message: "Hello World"
   })
 }
